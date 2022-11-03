362e4f50c4776fb8706701389cbea8e6ee7b76a3
osangu <osangu012@gmail.com>
2022-11-04 01:33:54 +0900

    UPDATE: UDPATE SECOND AUTH API 연결

--- app/core/admin/__init__.py
+++ app/core/admin/__init__.py
@@ -1,15 +1,20 @@
-from fastapi import status, APIRouter
+from fastapi import status, APIRouter, Depends
+
+from fastapi.security import OAuth2PasswordBearer
 
 from app.util.dao import session_scope
 
 from app.core.admin.dto.request.register import AdminRegisterRequest
-from app.core.admin.dto.request.second_auth import SecondAuthRequest
+from app.core.admin.dto.request.second_auth import SecondAuthRequest, UpdateSecondAuthRequest
 
 from app.core.admin.service.register import duc_admin_register
-from app.core.admin.service.second_auth import duc_second_auth_service
+from app.core.admin.service.second_auth import duc_second_auth_service, duc_update_second_auth_code_service
 
 from app.core.admin.service.register.register_impl import admin_register_impl
 from app.core.admin.service.second_auth.second_auth_impl import second_auth_service_impl
+from app.core.admin.service.second_auth.update_second_impl import update_second_auth_code_impl
+
+_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 admin_router = APIRouter(
     prefix='/admin'
@@ -30,4 +35,13 @@ def admin_second_auth(request: SecondAuthRequest):
     with session_scope() as session:
         return duc_second_auth_service(
             second_auth_service_impl, session, request.auth_code
+        )
+
+
+@admin_router.put('/second-auth')
+def update_second_auth_code(request: UpdateSecondAuthRequest,token: str = Depends(_oauth2_scheme)):
+    with session_scope() as session:
+        return duc_update_second_auth_code_service(
+            update_second_auth_code_impl, session,
+            token, request.auth_code, request.new_auth_code
         )
