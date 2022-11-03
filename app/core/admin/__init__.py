94d7390eca1bee709fc0f532a4cec85c39636f42
osangu <osangu012@gmail.com>
2022-11-04 00:57:59 +0900

    ADD: second auth 연동

--- app/core/admin/__init__.py
+++ app/core/admin/__init__.py
@@ -3,10 +3,13 @@ from fastapi import status, APIRouter
 from app.util.dao import session_scope
 
 from app.core.admin.dto.request.register import AdminRegisterRequest
+from app.core.admin.dto.request.second_auth import SecondAuthRequest
 
 from app.core.admin.service.register import duc_admin_register
+from app.core.admin.service.second_auth import duc_second_auth_service
 
 from app.core.admin.service.register.register_impl import admin_register_impl
+from app.core.admin.service.second_auth.second_auth_impl import second_auth_service_impl
 
 admin_router = APIRouter(
     prefix='/admin'
@@ -22,3 +25,9 @@ def admin_register(request: AdminRegisterRequest):
         )
 
 
+@admin_router.post('/second-auth')
+def admin_second_auth(request: SecondAuthRequest):
+    with session_scope() as session:
+        return duc_second_auth_service(
+            second_auth_service_impl, session, request.auth_code
+        )
