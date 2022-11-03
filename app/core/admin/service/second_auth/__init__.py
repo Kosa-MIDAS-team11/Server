9995de59d28f7afd9327a1fb025db0a27b73f2a5
osangu <osangu012@gmail.com>
2022-11-04 01:32:38 +0900

    UPDATE: update_second_auth abstract

--- app/core/admin/service/second_auth/__init__.py
+++ app/core/admin/service/second_auth/__init__.py
@@ -10,5 +10,17 @@ class SecondAuthService(ABC):
         pass
 
 
+class UpdateSecondAuthService(ABC):
+
+    @abstractmethod
+    def execute(self, session: Session, token: str, auth_code: str, new_auth_code: str):
+        pass
+
+
 def duc_second_auth_service(service_impl: SecondAuthService, session: Session, auth_code: str):
     return service_impl.execute(session, auth_code)
+
+
+def duc_update_second_auth_code_service(
+        service_impl: UpdateSecondAuthService, session: Session, token: str, auth_code: str, new_auth_code: str):
+    return service_impl.execute(session, token, auth_code, new_auth_code)
