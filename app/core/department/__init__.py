7527c9d879c6cce944f05b7ccb7b90fd968ad280
osangu <osangu012@gmail.com>
2022-11-04 03:36:13 +0900

    UPDATE: 부서 등록 API

--- app/core/department/__init__.py
+++ app/core/department/__init__.py
@@ -1,5 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from fastapi.security import OAuth2PasswordBearer
+
+from app.util.dao import session_scope
+
+from app.core.department.request.create import CreateDepartmentRequest
+
+from app.core.department.service.create import duc_create_department
+
+from app.core.department.service.create.crate_impl import create_department_impl
 
 department_router = APIRouter(
     prefix='/department'
 )
+
+_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@department_router.post('/', status_code=status.HTTP_201_CREATED)
+def create_department(reqeust: CreateDepartmentRequest, token: str = Depends(_oauth2_scheme)):
+    with session_scope() as session:
+        duc_create_department(create_department_impl, session, token, reqeust.name, reqeust.location)
